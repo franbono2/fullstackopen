32f46331404a86d31df58bd7d2a89c73d7fd98ed
franbono2 <franbono2@gmail.com>
2024-07-23 13:13:22 +0200

    Exercise 2.2 & 2.3

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,17 +1,28 @@
 const Course = ({ course }) => {
 
+  const totalNumExercises = () => {
+    const initialValue = 0
+    const total = course.parts.reduce((accumulator, current) => accumulator + current.exercises, initialValue)
+    return total
+  }
+
   return (
     <article>
       <header>
         <h1>{course.name}</h1>
       </header>
       {
-        course.parts.map( part => 
+        course.parts.map(part =>
           <p key={part.id}>
             {part.name} {part.exercises}
           </p>
         )
       }
+      <footer>
+        <strong>
+          Total of { totalNumExercises() } exercises
+        </strong>
+      </footer>
     </article>
   )
 }
