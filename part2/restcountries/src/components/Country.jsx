925c7fdedfe0a8a0de648aefbb2b9159d8cdd838
franbono2 <franbono2@gmail.com>
2024-07-31 20:14:29 +0200

    Exercise 2.20

--- part2/restcountries/src/components/Country.jsx
+++ part2/restcountries/src/components/Country.jsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from 'react'
+import Countries from '../services/Countries'
+
 const Country = ({ country }) => {
+  const [weather, setWeather] = useState(null)
 
   const languages = Object.values(country.languages)
+
+  useEffect(() => {
+    Countries.getWeather(country.capital)
+    .then((current) => {
+      setWeather(current)
+    })
+  }, [])
 
   return (
     <div>
@@ -18,6 +29,20 @@ const Country = ({ country }) => {
         src={country.flags.svg} 
         alt={country.flags.alt} 
       />
+      {
+        weather !== null &&
+        (
+          <section>
+            <h4>Weather in {country.capital}</h4>
+            <p>Temperature: {weather.main.temp} Celsius</p>
+            <img
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather.description} 
+            />
+            <p>Wind: {weather.wind.speed} m/s</p>
+          </section>
+        )
+      }
     </div>
   )
 }
--- part2/restcountries/src/services/Countries.js
+++ part2/restcountries/src/services/Countries.js
@@ -1,15 +1,41 @@
 import axios from 'axios'
+
 const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api'
+const openWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?'
+const openWeatherIconUrl = 'https://openweathermap.org/img/wn' 
+const openWeatherApiKey = import.meta.env.VITE_OPEN_WEATHER_KEY
+const units = 'metric'
+
 
 const getAll = async () => {
   try {
     const response = await axios.get(baseUrl + '/all')
     return response.data
   } catch (error) {
-    console.error('Error updating data:', error)
+    console.error('Error fetching data:', error)
+  }
+}
+
+const getWeather = async (city) => {
+  try {
+    const response = await axios.get(`${openWeatherUrl}q=${city}&units=${units}&appid=${openWeatherApiKey}`)
+    return response.data
+  } catch (error) {
+    console.error('Error fetching weather data:', error)
+  }
+}
+
+const getWeatherIcon = async (id) => {
+  try {
+    const response = await axios.get(`${openWeatherIconUrl}/${id}@2x.png`)
+    return response
+  } catch (error) {
+    console.error('Error fetching weather icon data:', error)
   }
 }
 
 export default { 
-  getAll
+  getAll,
+  getWeather,
+  getWeatherIcon
 }
