dfd1e7ead0c26c31e77a2c19fa136ffc9875b238
franbono2 <franbono2@gmail.com>
2024-08-01 18:21:46 +0200

    add server port env

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -123,7 +123,7 @@ app.put('/api/persons/:id', (req, res) => {
   res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
