f31b141a3ecd3a6a5b349173416f49c71c48d963
franbono2 <franbono2@gmail.com>
2024-08-01 14:12:33 +0200

    Exercise 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -45,6 +45,18 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (!person) {
+    res.status(404).send('Not found any person with the current id')
+    return
+  }
+
+  res.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
