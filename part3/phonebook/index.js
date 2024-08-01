1c121566e0688e6b974aaf30e0332c9ac4b18a5c
franbono2 <franbono2@gmail.com>
2024-08-01 16:49:26 +0200

    Exercise 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+morgan.token('person', function (req, res) { return JSON.stringify(res.req.body)})
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
 
 let persons = [
   { 
