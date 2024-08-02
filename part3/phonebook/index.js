71ff5366941b22560be48e0145772b9d2d34c3ca
franbono2 <franbono2@gmail.com>
2024-08-02 11:02:44 +0200

    Exercise 3.11

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -5,6 +5,7 @@ const app = express()
 
 morgan.token('person', function (req, res) { return JSON.stringify(res.req.body)})
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
