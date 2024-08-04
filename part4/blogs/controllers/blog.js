d939363080d9716e6e5b0c834e0d314e39c6b9d2
franbono2 <franbono2@gmail.com>
2024-08-04 19:03:21 +0200

    Exercise 4.13

--- part4/blogs/controllers/blog.js
+++ part4/blogs/controllers/blog.js
@@ -25,4 +25,17 @@ blogRouter.post('/', (request, response, next) => {
     .catch(error => next(error))
 })
 
+blogRouter.delete('/:id', async (request, response) => {
+  const id = request.params.id
+  await Blog.findByIdAndDelete(id)
+  response.status(204).end()
+})
+
+blogRouter.put('/:id', async (request, response) => {
+  const id = request.params.id
+  const newBlog = new Blog(request.body)
+  const updatedBlog = await Blog.findByIdAndUpdate(id, newBlog, {new: true})
+  response.json(updatedBlog)
+})
+
 module.exports = blogRouter
--- part4/blogs/tests/blogs_api.test.js
+++ part4/blogs/tests/blogs_api.test.js
@@ -31,91 +31,111 @@ beforeEach(async () => {
   await Promise.all(promiseArray)
 })
 
-test('GET all blogs in json', async () => {
-  await api.get('/api/blogs/')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
-
-test('GET all blogs', async () => {
-  const res = await api.get('/api/blogs/')
-   
-  assert.strictEqual(res.body.length, initialBlogs.length)
-})
-
-test('Check blogs id', async () => {
-  const res = await api.get('/api/blogs/')
-  res.body.forEach(blog => {
-    const id = Object.keys(blog).find(prop => prop === 'id')
-    assert.strictEqual(id, 'id')
+describe('Tests for api GET', () => {
+  test('GET all blogs in json', async () => {
+    await api.get('/api/blogs/')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+  
+  test('GET all blogs', async () => {
+    const res = await api.get('/api/blogs/')
+     
+    assert.strictEqual(res.body.length, initialBlogs.length)
   })
 })
 
-test('POST valid blog is added', async () => {
-  const newBlog = {
-    title: "Escape from FullStack",
-    author: "Pepe W. Navarro",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 12
-  }
-
-  await api.post('/api/blogs/')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await api.get('/api/blogs/')
-  assert.strictEqual(blogsAtEnd.body.length, initialBlogs.length + 1)
-
-  const contents = blogsAtEnd.body.map(blog => blog.title)
-  assert(contents.includes("Escape from FullStack"))
+describe('Check Id', () => {
+  test('Check blogs id', async () => {
+    const res = await api.get('/api/blogs/')
+    res.body.forEach(blog => {
+      const id = Object.keys(blog).find(prop => prop === 'id')
+      assert.strictEqual(id, 'id')
+    })
+  })
 })
 
-test('POST blog without likes contains 0 likes', async () => {
-  const newBlog = {
-    title: "Escape from FullStack",
-    author: "Pepe W. Navarro",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-  }
-
-  await api.post('/api/blogs/')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await api.get('/api/blogs/')
-  assert.strictEqual(blogsAtEnd.body.length, initialBlogs.length + 1)
-
-  const contents = blogsAtEnd.body.map(blog => blog.likes)
-  assert(contents.includes(0))
+describe('Test for api POST', () => {
+  test('POST valid blog is added', async () => {
+    const newBlog = {
+      title: "Escape from FullStack",
+      author: "Pepe W. Navarro",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 12
+    }
+  
+    await api.post('/api/blogs/')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const blogsAtEnd = await api.get('/api/blogs/')
+    assert.strictEqual(blogsAtEnd.body.length, initialBlogs.length + 1)
+  
+    const contents = blogsAtEnd.body.map(blog => blog.title)
+    assert(contents.includes("Escape from FullStack"))
+  })
+  
+  test('POST blog without likes contains 0 likes', async () => {
+    const newBlog = {
+      title: "Escape from FullStack",
+      author: "Pepe W. Navarro",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    }
+  
+    await api.post('/api/blogs/')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const blogsAtEnd = await api.get('/api/blogs/')
+    assert.strictEqual(blogsAtEnd.body.length, initialBlogs.length + 1)
+  
+    const contents = blogsAtEnd.body.map(blog => blog.likes)
+    assert(contents.includes(0))
+  })
+  
+  test('POST invalid blog (no author) is not added', async () => {
+    const newBlog = {
+      title: "Escape from FullStack",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    }
+  
+    await api.post('/api/blogs/')
+      .send(newBlog)
+      .expect(400)
+  
+    const blogsAtEnd = await api.get('/api/blogs/')
+    assert.strictEqual(blogsAtEnd.body.length, initialBlogs.length)
+  })
+  
+  test('POST invalid blog (no title) is not added', async () => {
+    const newBlog = {
+      author: "Pepe W. Navarro",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    }
+  
+    await api.post('/api/blogs/')
+      .send(newBlog)
+      .expect(400)
+  
+    const blogsAtEnd = await api.get('/api/blogs/')
+    assert.strictEqual(blogsAtEnd.body.length, initialBlogs.length)
+  })
 })
 
-test('POST invalid blog (no author) is not added', async () => {
-  const newBlog = {
-    title: "Escape from FullStack",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-  }
+describe('Test for api DELETE', () => {
+  test('DELETE blog by id', async () => {
+    const blogsAtStart = await api.get('/api/blogs/')
+    const blogToDelete = blogsAtStart.body[0]
+    await api.delete(`/api/blogs/${blogToDelete.id}/`).expect(204)
 
-  await api.post('/api/blogs/')
-    .send(newBlog)
-    .expect(400)
+    const blogsAfterDelete = await api.get('/api/blogs/')
+    assert.strictEqual(blogsAfterDelete.body.length, blogsAtStart.body.length - 1)
 
-  const blogsAtEnd = await api.get('/api/blogs/')
-  assert.strictEqual(blogsAtEnd.body.length, initialBlogs.length)
-})
-
-test('POST invalid blog (no title) is not added', async () => {
-  const newBlog = {
-    author: "Pepe W. Navarro",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-  }
-
-  await api.post('/api/blogs/')
-    .send(newBlog)
-    .expect(400)
-
-  const blogsAtEnd = await api.get('/api/blogs/')
-  assert.strictEqual(blogsAtEnd.body.length, initialBlogs.length)
+    const titles = blogsAfterDelete.body.map(blog => blog.title)
+    assert(!titles.includes(blogToDelete.title))
+  })
 })
 
 after(async () => {
