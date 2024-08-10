076e4c128c8e966cf5285198292b48e2b0b3fe9f
franbono2 <franbono2@gmail.com>
2024-08-10 17:06:20 +0200

    Exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,25 +1,40 @@
 import '@testing-library/jest-dom'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('blog shows author and title', () => {
-  const blog = {
-    title: "Test Blog",
-    author: "Vitest",
-    url: "0.0.0.0",
-    likes: 0,
-    user: {
-      username: "Paco",
-      name: "Paco Gonzalez"
-    }
-  }
+const blog = {
+  title: "Test Blog",
+  author: "Vitest",
+  url: "0.0.0.0",
+  likes: 0,
+  user: {
+    username: "Paco",
+    name: "Paco Gonzalez"
+}}
 
-  const { container } = render(<Blog blog={blog} />)
+describe('Blogs tests <Blog />', () => {
+  let container
 
-  const div = container.querySelector('.blog')
-  expect(div).toHaveTextContent(`${blog.title}`)
-  expect(div).toHaveTextContent(`${blog.author}`)
+  beforeEach(() => {
+    container = render(<Blog blog={blog} />).container
+  })
 
-  const hiddenDiv = container.querySelector('.startHidden')
-  expect(hiddenDiv).toHaveStyle('display: none')
+  test('blog shows author and title at start', () => {
+    const div = container.querySelector('.blog')
+    expect(div).toHaveTextContent(`${blog.title}`)
+    expect(div).toHaveTextContent(`${blog.author}`)
+  
+    const hiddenDiv = container.querySelector('.startHidden')
+    expect(hiddenDiv).toHaveStyle('display: none')
+  })
+
+  test('after clicking the button, url and likes are displayed', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const hiddenDiv = container.querySelector('.startHidden')
+    expect(hiddenDiv).not.toHaveStyle('display: none')
+  })
 })
