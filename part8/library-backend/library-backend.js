764c30e03a004e0602d0aa3c3b0e7874e820ff1e
franbono2 <franbono2@gmail.com>
2024-09-02 16:49:17 +0200

    Exercise 8.5

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -97,7 +97,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int
     authorCount: Int
-    allBooks(author: String!): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -106,7 +106,17 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: (_root, args) => books.filter((b) => b.author === args.author),
+    allBooks: (_root, args) => {
+      if (!args.author && !args.genre) return books;
+      if (!args.genre) {
+        return books.filter((b) => b.author === args.author);
+      }
+      if (!args.author) {
+        return books.filter((b) => b.genres.includes(args.genre));
+      }
+      const booksByAuthor = books.filter((b) => b.author === args.author);
+      return booksByAuthor.filter((b) => b.genres.includes(args.genre));
+    },
     allAuthors: () => authors,
   },
   Author: {
